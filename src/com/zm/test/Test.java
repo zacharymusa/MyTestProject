a11f3ecebfbe320ee1b00079795a1f2924887190
zacharymusa <zacharymusa@yahoo.com>
2014-04-29 18:26:08 -0700

    Test4

--- src/com/zm/test/Test.java
+++ src/com/zm/test/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" Testing ...");
-		
+		System.out.println(" Testing 1...");
+		System.out.println(" Testing2 ...");
 	}
 
 }
